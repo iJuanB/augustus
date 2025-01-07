531049b8c2024be7a6c3f6393066133746fb265f
iJuanB <73082431+iJuanB@users.noreply.github.com>
2025-01-06 19:02:53 -0500

    Augustus 2.0

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from langchain_cohere import CohereEmbeddings
-from langchain_groq import ChatGroq
-from pinecone import Pinecone
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
-from pinecone_plugins.assistant.models.chat import Message
-from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from crewai_tools import SerperDevTool
+from crewai import Agent, Task, Crew, LLM
+
+from dotenv import load_dotenv
+load_dotenv()
+
+llm = LLM(model="gemini/gemini-2.0-flash-exp", temperature=0.1)
 
 import streamlit as st
 import os
@@ -14,49 +13,53 @@ os.environ["GROQ_API_KEY"] = 'gsk_OwKTEA30iaovfyG10CVdWGdyb3FY0NnnwaZ6z5r4hIQZqz
 os.environ["COHERE_API_KEY"] = 'hgNPboby7mr6AErA1L7CRP7P969JquZZE2XnGKoW'
 os.environ["PINECONE_API_KEY"] = '44c0e7c6-a11c-4c77-aee5-a951e43cd2a1'
 
-pc = Pinecone()
-
-llm = ChatGroq(model='llama-3.1-70b-versatile', temperature=0.1)
-
-embeddings = CohereEmbeddings(
-    model="embed-multilingual-v3.0",
+tool = SerperDevTool(
+    country="col",
+    locale="col",
+    location="Colombia",
+    n_results=2,
 )
 
-assistant = pc.assistant.Assistant(
-    assistant_name="prueba", 
+augustus = Agent(
+    role='Experto en propiedad horizontal',
+    goal='Debes responder preguntas de propiedad horizontal en Colombia',
+    backstory='Eres un experto en propiedad horizontal en Colombia, debes responder de manera precisa. Tu nombre es Augustus',
+    tools=[tool],
+    llm=llm,
+    verbose=True
 )
 
-def assistant_prompt():
-    prompt = ChatPromptTemplate.from_messages(
-    ("human", """ # Rol
-     Tu nombre es Augustus. Eres un asistente virtual especializado en leyes, específicamente en el campo de propiedad horizontal en Colombia. Tienes que resolver conflictos de propiedad horizontal basándote en las leyes de Colombia, y debes responder en base al contexto.\n\n
+def augustus_task(prompt): 
+    return Task(
+    description=f"""
+    Eres un experto de propiedad horizontal en Colombia
+
+    Debes responder a la siguiente pregunta del usuario: {prompt}
     
-    Question: {question}  Contexto: {context}
-    """))
-    return prompt
-
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
+    si puedes responder con base a tus conocimientos hazlo, si no tienes los conocimientos entonces usa la herramienta para buscar en internet. """,
+    agent=augustus,
+    expected_output='Respuesta precisa'
+)
 
 st.header('Augustus')
 
 user_prompt = st.text_input('Preguntale a Augustus sobre cualquier tema de Propiedad Horizontal.')
 
 if user_prompt:
-    msg = Message(content=user_prompt)
-    resp = assistant.chat_completions(messages=[msg])
-
-    prompt = assistant_prompt()
-
-
-    response_assitant = resp['choices'][0]['message']['content']
-
-    rag_chain = (
-        prompt
-        | llm
-        | StrOutputParser()
-    )
-
-    response = rag_chain.invoke({'context': response_assitant, 'question': user_prompt})
-
-    st.write(response)
+    question = Message(content=user_prompt)
+    
+    augustus_crew = Crew(
+    agents=[augustus],
+    tasks=[augustus_task(question)],
+    verbose=True,
+    memory=True,
+    embedder={
+        "provider": "cohere",
+        "config": {
+            "api_key": "hgNPboby7mr6AErA1L7CRP7P969JquZZE2XnGKoW",
+            "model_name": "embed-multilingual-v3.0"
+        }
+    }
+)
+    response = augustus_crew.kickoff()
+    st.write(response.raw)
